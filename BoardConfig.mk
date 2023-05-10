0ebb116a6d21043d7b7135d6d21b026d713bbd7c
raghavt20 <raghavt20@aospa.co>
2023-05-10 17:01:43 +0530

    violet: rootdir: Commonize fstab.qcom
    We are now using retrofit dynamic partitioning on violet, we can now
    commonize the fstab.
    
    Change-Id: Ib7128633bbe89b7369c0d3244218a5cd8db85f54
    Signed-off-by: raghavt20 <raghavt20@aospa.co>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,8 +48,5 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno612
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
-# Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
-
 # Inherit from the proprietary version
 include vendor/xiaomi/violet/BoardConfigVendor.mk
