7cef03be0b2e574ed17429d86057985ae0b67645
raghavt20 <raghavt20@aospa.co>
2023-05-29 02:21:26 +0530

    violet: Inherit common extract-files shell script
    Change-Id: Id9b5dc173c7a009d014d4087bffedccda8b2fc4a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -7,31 +7,10 @@
 
 set -e
 
-DEVICE=violet
-VENDOR=xiaomi
+export DEVICE=violet
+export DEVICE_COMMON=sm6150-common
+export VENDOR=xiaomi
 
-INITIAL_COPYRIGHT_YEAR=2019
+export INITIAL_COPYRIGHT_YEAR=2019
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$ANDROID_ROOT"
-
-# Copyright headers and guards
-write_headers
-
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
-
-# Finish
-write_footers
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
