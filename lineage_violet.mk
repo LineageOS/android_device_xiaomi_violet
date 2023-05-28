36d54006bb4170e7edb3785b4a6a9656b7caa6f9
raghavt20 <raghavt20@aospa.co>
2023-05-29 03:32:55 +0530

    violet: Refactor the code for inheriting sm6150 common
    Restructurize the tree [inline tree structure with sm6150-common trees]
    
    Change-Id: I4192184f1a4d2bc054254741265ef89a1572c0f2

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -5,13 +5,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-TARGET_SCREEN_HEIGHT := 2340
-TARGET_SCREEN_WIDTH := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
