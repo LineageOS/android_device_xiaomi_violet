38545691a212e7ed5f84c5571a554c06790d300a
luca020400 <luca.stefani.ge1@gmail.com>
2023-06-01 23:34:28 +0530

    violet: overlay-lineage: Specify device has a notch
    Change-Id: I325e3defdf38b4895944b794e6dc4987f885daae

--- device.mk
+++ device.mk
@@ -70,7 +70,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Power
 PRODUCT_COPY_FILES += \
