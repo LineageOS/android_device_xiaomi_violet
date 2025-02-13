9221f42206bd3940b09a87b67e5746f0bd679afa
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 15:22:18 +0800

    [DNM] Haptic
    Change-Id: I7f18f1bb2344a2feed1e45b4d13fda52793a48d5

--- device.mk
+++ device.mk
@@ -63,6 +63,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    HapticOverlay
+
 # Power
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
